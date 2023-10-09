bafa9a26342460a59763dcf73de5c28ed831f7b4
rougho <rohi@rgho.de>
2023-10-09 14:13:24 +0200

    static added

--- rgho/settings.py
+++ rgho/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+
+# This is only if you're deploying, not required for development.
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
