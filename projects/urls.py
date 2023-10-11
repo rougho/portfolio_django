e64a3b931a1b62a498055dc84275bb7c54bcae48
rougho <rohi@rgho.de>
2023-10-11 15:49:10 +0200

    project detail routed

--- projects/urls.py
+++ projects/urls.py
@@ -3,15 +3,17 @@ from projects import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = "projects"
+
 urlpatterns = [
     path('', views.home),
-    path('sign_up/', views.sign_up),
-    path('sign_in/',views.sign_in),
-    path('contact/',views.contact),
-    path('about/',views.about),
-    path('download/',views.download),
-    path('projects/', views.projects),
-    path('projects/<int:pk>', views.project_detail)
+    path('sign_up/', views.sign_up,name="signup"),
+    path('sign_in/',views.sign_in, name="signin"),
+    path('contact/',views.contact, name="contact"),
+    path('about/',views.about, name="about"),
+    path('download/',views.download, name="download"),
+    path('projects/', views.projects, name="projects"),
+    path('projects/<int:pk>', views.project_detail, name="project_detail")
 ]
 
 if settings.DEBUG:
