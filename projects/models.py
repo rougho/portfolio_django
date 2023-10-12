9aaca50dafc8ba0be8dd6a6b79c00f4063f024af
rougho <rohi@rgho.de>
2023-10-12 17:20:35 +0200

    migration named && designed a bit

--- projects/models.py
+++ projects/models.py
@@ -6,4 +6,5 @@ class Project(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
     technology = models.CharField(max_length=100)
+    links = models.URLField(max_length=200, blank=True, null=True)
     image = models.CharField(max_length=100)
